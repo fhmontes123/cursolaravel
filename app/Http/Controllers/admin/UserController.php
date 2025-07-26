96ded76c729d100330e586e94d4579f6b14ee041
fhmontes123 <hugo.educomser@gmail.com>
2025-07-25 21:00:25 -0400

    Laravel 16 > Añadir validaciones

--- app/Http/Controllers/admin/UserController.php
+++ app/Http/Controllers/admin/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\UserStoreRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -30,7 +31,7 @@ class UserController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(UserStoreRequest $request)
     {
         // dd($request);
         $user = new User($request->all());
